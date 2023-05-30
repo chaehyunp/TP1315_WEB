6dc35005045d201fb3a5767a5a311bc421adcc40
chhue <chhue96@gmail.com>
2023-05-30 17:51:34 +0900

    about

--- js/menu_hamburger.js
+++ js/menu_hamburger.js
@@ -1,10 +1,23 @@
-var burger = $('.menu-trigger');
+// var burger = $('.menu-trigger');
 
-burger.each(function(index){
-  var $this = $(this);
+// burger.each(function(index){
+//   var $this = $(this);
   
-  $this.on('click', function(e){
-    e.preventDefault();
-    $(this).toggleClass('active-' + (index+1));
-  })
+//   $this.on('click', function(e){
+//     e.preventDefault();
+//     $(this).toggleClass('active-' + (index+1));
+//   })
+// });
+
+var hamburger = document.getElementById('hamburger');
+var navbox = document.getElementById('navbox');
+
+hamburger.addEventListener('click', function() {
+  if (navbox.classList.contains('open')) {
+    navbox.classList.add('close');
+    // navbox.remove('open');
+  } else {
+    navbox.classList.add('open');
+    // navbox.remove('close');
+  }
 });
